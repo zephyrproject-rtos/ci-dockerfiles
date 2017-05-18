d5ddf5522fdb161b7dfb95a3e7c1b0b45ea91e5e
Anas Nashif <anas.nashif@intel.com>
2017-05-17 20:24:50 -0400

    shippable: update to latest SDK: 0.9.1
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -e
-wget https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/0.9/zephyr-sdk-0.9-setup.run
-chmod +x zephyr-sdk-0.9-setup.run
-./zephyr-sdk-0.9-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-0.9
-rm zephyr-sdk-0.9-setup.run
+
+VERSION=0.9.1-rc1
+VERSION1=0.9.1-rc
+wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
+chmod +x zephyr-sdk-${VERSION1}-setup.run
+./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-0.9.1
+rm zephyr-sdk-${VERSION1}-setup.run
