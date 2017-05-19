7af7e6e5b142ad37557eb7097c4132e9c2e9c4a4
Anas Nashif <anas.nashif@intel.com>
2017-05-19 00:24:36 -0400

    ci: install sphinx 1.5.5
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_deps.sh
+++ shippable/install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 echo "==================== Installing zephyr dependencies ==================="
 apt-get install git make gcc g++ python3-ply python3-yaml python-yaml doxygen device-tree-compiler
-pip install awscli sphinx breathe sphinx_rtd_theme junit2html
+pip install awscli breathe==4.6.0 sphinx==1.5.5 sphinx_rtd_theme junit2html
 
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/ccache_3.3.3-1_amd64.deb
