d5f47dda317c3bdda4778660d61bce81bbe25a1e
Anas Nashif <anas.nashif@intel.com>
2017-05-20 12:35:15 -0400

    ci: install clang from using apt
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install.sh
+++ shippable/install.sh
@@ -8,13 +8,13 @@ apt-get clean
 apt-get update
 echo "======================== Cleaned apt-get ======================"
 
-echo "======================= Installing gcc 6 ======================"
 add-apt-repository ppa:ubuntu-toolchain-r/test
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-3.9 main"
 apt-get update
-apt-get install gcc-6 g++-6
+apt-get install gcc-6 g++-6 clang-3.9 lldb-3.9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-6 50
 gcc --version
-echo "================== Successfully Installed gcc 6 ==============="
 
 
