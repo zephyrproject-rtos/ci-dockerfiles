03e58f6f3a3e6f3c28e33cc10a61bdb855bd38ec
Anas Nashif <anas.nashif@intel.com>
2017-07-19 23:45:48 +0300

    install additional python modules

--- shippable/install_deps.sh
+++ shippable/install_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 echo "==================== Installing zephyr dependencies ==================="
 apt-get install git make gcc g++ python3-ply python3-yaml python-yaml doxygen device-tree-compiler qemu
-pip install awscli breathe==4.6.0 sphinx==1.5.5 sphinx_rtd_theme junit2html
+apt-get install python3-pip
+pip3 install awscli breathe==4.6.0 sphinx==1.5.5 sphinx_rtd_theme junit2html
+pip3 install pyelftools==0.24 pykwalify
 
 
 wget https://launchpad.net/ubuntu/+archive/primary/+files/ccache_3.3.3-1_amd64.deb
