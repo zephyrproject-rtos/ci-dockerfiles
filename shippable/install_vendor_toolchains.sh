b829d9ced1ebd99623b3d8a38e17175d6695d2bd
Anas Nashif <anas.nashif@intel.com>
2017-07-20 00:11:41 +0300

    run wget in quiet mode
    also untar without verbosity
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_vendor_toolchains.sh
+++ shippable/install_vendor_toolchains.sh
@@ -1,18 +1,18 @@
 
 
-wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
-tar xvf gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
+wget -q https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/6_1-2017q1/gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
+tar xf gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
 rm -f gcc-arm-none-eabi-6-2017-q1-update-linux.tar.bz2
 mkdir /opt/toolchain
 mv gcc-arm-none-eabi-6-2017-q1-update /opt/toolchain/arm-none-eabi
 
-wget http://registrationcenter-download.intel.com/akdlm/irc_nas/9572/issm-toolchain-linux-2017-02-07.tar.gz
-tar xvf issm-toolchain-linux-2017-02-07.tar.gz
+wget -q http://registrationcenter-download.intel.com/akdlm/irc_nas/9572/issm-toolchain-linux-2017-02-07.tar.gz
+tar xf issm-toolchain-linux-2017-02-07.tar.gz
 rm issm-toolchain-linux-2017-02-07.tar.gz
 mv issm-toolchain-linux-2017-02-07 /opt/toolchain/issm
 
-wget https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2016.09-release/arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-tar xvf arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
+wget -q https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2016.09-release/arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
+tar xf arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
 rm arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
 mv arc_gnu_2016.09_prebuilt_elf32_le_linux_install /opt/toolchain/arc-elf32
 
