e9777ad48cbc9b1f4c4c2368548d9f2bbbb7e223
Anas Nashif <anas.nashif@intel.com>
2017-08-14 10:07:36 -0400

    disable installation of unused ARC toolchain
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_vendor_toolchains.sh
+++ shippable/install_vendor_toolchains.sh
@@ -11,10 +11,10 @@ tar xf issm-toolchain-linux-2017-02-07.tar.gz
 rm issm-toolchain-linux-2017-02-07.tar.gz
 mv issm-toolchain-linux-2017-02-07 /opt/toolchain/issm
 
-wget -q https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2016.09-release/arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-tar xf arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-rm arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-mv arc_gnu_2016.09_prebuilt_elf32_le_linux_install /opt/toolchain/arc-elf32
+#wget -q https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2016.09-release/arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
+#tar xf arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
+#rm arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
+#mv arc_gnu_2016.09_prebuilt_elf32_le_linux_install /opt/toolchain/arc-elf32
 
 
 
