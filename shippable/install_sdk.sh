5533eab08813ac42065a47db00bc0fbf24699c7b
Anas Nashif <anas.nashif@intel.com>
2017-08-29 08:27:10 -0400

    pull the 0.9.1 SDK
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -e
 
-VERSION=0.9.1-rc1
-VERSION1=0.9.1-rc
+VERSION=0.9.1
+VERSION1=0.9.1
 wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
 chmod +x zephyr-sdk-${VERSION1}-setup.run
-./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-0.9.1
+./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
