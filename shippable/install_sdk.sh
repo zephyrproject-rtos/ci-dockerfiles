89e18a366eb5087d2cbcfefcdfbace8ffb1413d6
Anas Nashif <anas.nashif@intel.com>
2017-09-02 07:51:12 -0400

    install beta SDK and cleanup downloads
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -6,3 +6,10 @@ wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/dow
 chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
+
+BETA_VERSION=0.9.2-rc2
+BETA_VERSION1=0.9.2-rc2
+wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${BETA_VERSION}/zephyr-sdk-${BETA_VERSION1}-setup.run
+chmod +x zephyr-sdk-${BETA_VERSION1}-setup.run
+./zephyr-sdk-${BETA_VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${BETA_VERSION}
+rm zephyr-sdk-${BETA_VERSION1}-setup.run
