63ad59a38f4a798d4f477169467b3b8ee680f7bd
Anas Nashif <anas.nashif@intel.com>
2017-09-11 15:19:00 -0400

    update with SDK 0.9.2-rc4
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -7,8 +7,8 @@ chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
 
-BETA_VERSION=0.9.2-rc2
-BETA_VERSION1=0.9.2-rc2
+BETA_VERSION=0.9.2-rc4
+BETA_VERSION1=0.9.2-rc4
 wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${BETA_VERSION}/zephyr-sdk-${BETA_VERSION1}-setup.run
 chmod +x zephyr-sdk-${BETA_VERSION1}-setup.run
 ./zephyr-sdk-${BETA_VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${BETA_VERSION}
