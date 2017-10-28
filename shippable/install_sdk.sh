351b7e388999e97c83f6aa6d64e37e6e0519185a
Anas Nashif <anas.nashif@intel.com>
2017-10-28 14:25:44 -0400

    use Zephyr SDK 0.9.2
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,15 +1,8 @@
 #!/bin/sh -e
 
-VERSION=0.9.1
-VERSION1=0.9.1
+VERSION=0.9.2
+VERSION1=0.9.2
 wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
 chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
-
-BETA_VERSION=0.9.2-rc4
-BETA_VERSION1=0.9.2-rc4
-wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${BETA_VERSION}/zephyr-sdk-${BETA_VERSION1}-setup.run
-chmod +x zephyr-sdk-${BETA_VERSION1}-setup.run
-./zephyr-sdk-${BETA_VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${BETA_VERSION}
-rm zephyr-sdk-${BETA_VERSION1}-setup.run
