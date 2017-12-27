bfff23c05e6adc7fe3e691d675f655985ca16def
Anas Nashif <anas.nashif@intel.com>
2017-12-27 14:58:15 -0500

    update image dependencies
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_deps.sh
+++ shippable/install_deps.sh
@@ -3,12 +3,12 @@ echo "==================== Installing zephyr dependencies ==================="
 apt-get install git make gcc g++ python3-ply python3-yaml python-yaml doxygen device-tree-compiler qemu
 apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm libreadline-dev makeself p7zip-full cpio gperf
 apt-get install python3-pip
-apt-get install gcc-6-multilib gcovr
+apt-get install gcc-6-multilib gcovr valgrind
 
 # for net-tools
 apt-get install libglib2.0-dev libpcap-dev
-pip3 install awscli breathe==4.6.0 sphinx==1.5.5 sphinx_rtd_theme junit2html
-pip3 install pyelftools==0.24 pykwalify sh gitlint==0.8.2
+pip3 install awscli breathe==4.7.3 sphinx==1.6.5 docutils==0.14 sphinx_rtd_theme junit2html
+pip3 install pyelftools==0.24 pykwalify sh gitlint==0.9.0
 
 
 wget -q https://launchpad.net/ubuntu/+archive/primary/+files/ccache_3.3.3-1_amd64.deb
