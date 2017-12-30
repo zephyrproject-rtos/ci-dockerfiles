cbe11a18f6bf44420c3b57257c86cb8b1a0c66e1
Anas Nashif <anas.nashif@intel.com>
2017-12-30 14:07:26 -0500

    docker: add ninja-build
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_deps.sh
+++ shippable/install_deps.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 echo "==================== Installing zephyr dependencies ==================="
-apt-get install git make gcc g++ python3-ply python3-yaml python-yaml doxygen device-tree-compiler qemu
+apt-get install git make gcc g++ python3-ply python3-yaml doxygen device-tree-compiler qemu
 apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm libreadline-dev makeself p7zip-full cpio gperf
 apt-get install python3-pip
-apt-get install gcc-6-multilib gcovr valgrind
+apt-get install gcc-6-multilib gcovr valgrind ninja-build
 
 # for net-tools
 apt-get install libglib2.0-dev libpcap-dev
