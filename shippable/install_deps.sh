e291dbe83e3b97864aca4027073422782f5fc818
Anas Nashif <anas.nashif@intel.com>
2017-12-31 10:48:40 -0500

    deps: add lcov for code coverage
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_deps.sh
+++ shippable/install_deps.sh
@@ -3,7 +3,7 @@ echo "==================== Installing zephyr dependencies ==================="
 apt-get install git make gcc g++ python3-ply python3-yaml doxygen device-tree-compiler qemu
 apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm libreadline-dev makeself p7zip-full cpio gperf
 apt-get install python3-pip
-apt-get install gcc-6-multilib gcovr valgrind ninja-build
+apt-get install gcc-6-multilib gcovr valgrind ninja-build lcov
 
 # for net-tools
 apt-get install libglib2.0-dev libpcap-dev
