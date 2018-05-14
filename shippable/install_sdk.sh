dcc735f3b0bb9ec534fd7f8e4240b40b745810ba
Anas Nashif <anas.nashif@intel.com>
2018-05-14 19:37:58 +0300

    Update SDK to 0.9.3

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-VERSION=0.9.2
-VERSION1=0.9.2
+VERSION=0.9.3
+VERSION1=0.9.3
 wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
 chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
