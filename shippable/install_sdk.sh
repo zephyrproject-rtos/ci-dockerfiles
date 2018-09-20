a5676838602616f5c17886a1f937883aafba97b0
Anas Nashif <anas.nashif@intel.com>
2018-09-20 08:12:40 -0500

    sdk: install SDK 0.9.4-rc3 for testing
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -6,3 +6,10 @@ wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/dow
 chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
+
+VERSION=0.9.4-rc3
+VERSION1=0.9.4-rc3
+wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
+chmod +x zephyr-sdk-${VERSION1}-setup.run
+./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+rm zephyr-sdk-${VERSION1}-setup.run
