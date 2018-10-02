d4e4d86c4f0495459dccbb9d54a8f65a1a7ace44
Alberto Escolar Piedras <alpi@oticon.com>
2018-10-02 11:51:16 +0200

    add bsim v1.0.1

---
+++ shippable/install_bsim.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+echo "==================== Installing BabbleSim ==================="
+export BSIM_VERSION=v1.0.1
+mkdir -p /opt/bsim
+cd /opt/bsim
+curl https://storage.googleapis.com/git-repo-downloads/repo > ./repo  && chmod a+x ./repo 
+./repo init -u https://github.com/BabbleSim/manifest.git -m zephyr_docker.xml -b $BSIM_VERSION --depth 1
+./repo sync
+make everything -j 8
+echo $BSIM_VERSION > ./version
+echo "=============== Successfully Installed BabbleSim ============"
