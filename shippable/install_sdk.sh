eba14f161accbd05e4fd88a1d1636ecfa6587ec9
Anas Nashif <anas.nashif@intel.com>
2018-11-02 14:56:38 -0400

    sdk: install 0.9.4 version of the SDK
    Move to the new SDK.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
-VERSION=0.9.3
-VERSION1=0.9.3
+VERSION=0.9.4
+VERSION1=0.9.4
 wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
 chmod +x zephyr-sdk-${VERSION1}-setup.run
 ./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION1}-setup.run
 
-VERSION=0.9.4-rc5
-VERSION1=0.9.4-rc5
-wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
-chmod +x zephyr-sdk-${VERSION1}-setup.run
-./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
-rm zephyr-sdk-${VERSION1}-setup.run
+#VERSION=0.9.4-rc5
+#VERSION1=0.9.4-rc5
+#wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
+#chmod +x zephyr-sdk-${VERSION1}-setup.run
+#./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+#rm zephyr-sdk-${VERSION1}-setup.run
