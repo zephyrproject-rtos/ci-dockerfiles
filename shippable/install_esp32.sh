499a989ac141eb8c836051ae4466276966ba02af
Anas Nashif <anas.nashif@intel.com>
2018-11-17 23:54:26 -0500

    new python packages, version updates
    - Install cmake 3.8.2 to match required version in Zephyr
    - Do not install ISSM toolchain
    - Install west, pygithub and junitparser
    - Use requirements.txt file from tree to keep things synced
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_esp32.sh
+++ shippable/install_esp32.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-
-
 mkdir -p /opt/toolchain/esp32
 cd /opt/toolchain/esp32
 wget https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-80-g6c4433a-5.2.0.tar.gz
--- shippable/install_vendor_toolchains.sh
+++ shippable/install_vendor_toolchains.sh
@@ -6,15 +6,3 @@ rm -f gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
 mkdir /opt/toolchain
 mv gcc-arm-none-eabi-7-2018-q2-update /opt/toolchain/arm-none-eabi
 
-wget -q http://registrationcenter-download.intel.com/akdlm/irc_nas/9572/issm-toolchain-linux-2017-02-07.tar.gz
-tar xf issm-toolchain-linux-2017-02-07.tar.gz
-rm issm-toolchain-linux-2017-02-07.tar.gz
-mv issm-toolchain-linux-2017-02-07 /opt/toolchain/issm
-
-#wget -q https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2016.09-release/arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-#tar xf arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-#rm arc_gnu_2016.09_prebuilt_elf32_le_linux_install.tar.gz
-#mv arc_gnu_2016.09_prebuilt_elf32_le_linux_install /opt/toolchain/arc-elf32
-
-
-
