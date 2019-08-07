e2cd09fd50e8fde7c39cc199923209efc1862942
Kumar Gala <kumar.gala@linaro.org>
2019-08-06 21:02:03 -0500

    Install SDK 0.10.2-rc2 into docker image
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk_ng.sh
+++ shippable/install_sdk_ng.sh
@@ -7,3 +7,10 @@ chmod +x zephyr-sdk-${VERSION}-setup.run
 
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-setup.run
+
+
+wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.10.2-rc2/zephyr-sdk-0.10.2-rc2-setup.run
+chmod +x zephyr-sdk-0.10.2-rc2-setup.run
+
+./zephyr-sdk-0.10.2-rc2-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-v0.10.2-rc2
+rm zephyr-sdk-0.10.2-rc2-setup.run
