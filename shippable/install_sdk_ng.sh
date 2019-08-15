becaa594345d2e75ebeea44c2bad3783fea022cc
Kumar Gala <kumar.gala@linaro.org>
2019-08-15 18:02:45 -0500

    Bump SDK-NG version to 0.10.3
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk_ng.sh
+++ shippable/install_sdk_ng.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-VERSION=0.10.2
+VERSION=0.10.3
 
 wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
 chmod +x zephyr-sdk-${VERSION}-setup.run
