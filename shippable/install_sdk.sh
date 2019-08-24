43f7589f9f37681b3003de4a08cfe330d212488e
Anas Nashif <anas.nashif@intel.com>
2019-08-24 11:51:18 -0400

    docker: update dependencies
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 
 VERSION=0.9.5
-VERSION1=0.9.5
-wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
-chmod +x zephyr-sdk-${VERSION1}-setup.run
-./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
-rm zephyr-sdk-${VERSION1}-setup.run
+wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION}-setup.run
+chmod +x zephyr-sdk-${VERSION}-setup.run
+./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+rm zephyr-sdk-${VERSION}-setup.run
 
-#VERSION=0.9.4-rc5
-#VERSION1=0.9.4-rc5
-#wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION1}-setup.run
-#chmod +x zephyr-sdk-${VERSION1}-setup.run
-#./zephyr-sdk-${VERSION1}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
-#rm zephyr-sdk-${VERSION1}-setup.run
+VERSION=0.10.3
+wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
+chmod +x zephyr-sdk-${VERSION}-setup.run
+
+./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+rm zephyr-sdk-${VERSION}-setup.run
+
--- shippable/install_sdk_ng.sh
+++ shippable/install_sdk_ng.sh
@@ -1,9 +1,2 @@
 #!/bin/sh -e
 
-VERSION=0.10.3
-
-wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
-chmod +x zephyr-sdk-${VERSION}-setup.run
-
-./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
-rm zephyr-sdk-${VERSION}-setup.run
