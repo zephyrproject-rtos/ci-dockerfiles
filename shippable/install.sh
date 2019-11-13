7a7df00f5b882a5eb137a46fe01c0a72d30dbd5e
Kumar Gala <kumar.gala@linaro.org>
2019-11-13 05:09:44 -0600

    Remove sccache
    We aren't using it, lets remove it.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install.sh
+++ shippable/install.sh
@@ -44,8 +44,7 @@ apt-get install -y \
 	ninja-build \
 	lcov \
 	libglib2.0-dev \
-	libpcap-dev \
-	cargo
+	libpcap-dev
 
 pip3 install -r https://raw.githubusercontent.com/zephyrproject-rtos/zephyr/master/scripts/requirements.txt
 pip3 install awscli PyGithub junitparser pylint
@@ -64,6 +63,4 @@ update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 50 --slave /usr/bi
 update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-6 50
 gcc --version
 
-# install sccache
-cargo install --root=/usr/local sccache
 
