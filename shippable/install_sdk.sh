f1528916bba284d6ba581430cdda783ae6262378
Kumar Gala <kumar.gala@linaro.org>
2019-11-13 06:12:46 -0600

    install_sdk: remove 0.9.5 toolchain
    We use 0.10.3 for CI purposes.  Remove 0.9.5 as its not used.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -e
 
-VERSION=0.9.5
-wget --quiet  https://github.com/zephyrproject-rtos/meta-zephyr-sdk/releases/download/${VERSION}/zephyr-sdk-${VERSION}-setup.run
-chmod +x zephyr-sdk-${VERSION}-setup.run
-./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
-rm zephyr-sdk-${VERSION}-setup.run
-
 VERSION=0.10.3
 wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
 chmod +x zephyr-sdk-${VERSION}-setup.run
