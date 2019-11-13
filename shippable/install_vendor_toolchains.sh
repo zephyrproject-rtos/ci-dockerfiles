f765436043b7fa04977dfbef152249194442e04a
Kumar Gala <kumar.gala@linaro.org>
2019-11-13 06:12:11 -0600

    install_vendor_toolchains: remove unused toolchain
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_vendor_toolchains.sh
+++ shippable/install_vendor_toolchains.sh
@@ -1,13 +1,4 @@
-# Install gnu-mcu-eclipse
-export ECLIPSE_GCC_ARM_VER=8.2.1-1.4
-export ECLIPSE_GCC_ARM_BUILD=20190214-0604
-
 mkdir -p /opt/toolchain
-
-wget -O gcc.tgz -q https://github.com/gnu-mcu-eclipse/arm-none-eabi-gcc/releases/download/v$ECLIPSE_GCC_ARM_VER/gnu-mcu-eclipse-arm-none-eabi-gcc-$ECLIPSE_GCC_ARM_VER-$ECLIPSE_GCC_ARM_BUILD-centos64.tgz
-tar xf gcc.tgz
-rm -f gcc.tgz
-mv gnu-mcu-eclipse/arm-none-eabi-gcc/$ECLIPSE_GCC_ARM_VER-$ECLIPSE_GCC_ARM_BUILD/ /opt/toolchain/gnu-mcu-eclipse
 
 # Install the official GNU ARM Embedded
 wget -q https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-linux.tar.bz2
