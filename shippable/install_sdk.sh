78c1502a16a235b4c4f7a8dd17ccf46119248b1e
Kumar Gala <kumar.gala@linaro.org>
2020-02-01 14:27:30 -0600

    test sdk pr-178
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -7,8 +7,8 @@ chmod +x zephyr-sdk-${VERSION}-setup.run
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-setup.run
 
-VERSION=0.11.0
-wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
+VERSION=0.11.0-pr-178
+wget -q https://builds.zephyrproject.org/zephyrproject-rtos/sdk-ng/178/zephyr-sdk-0.11.0-pr-178-setup.run
 chmod +x zephyr-sdk-${VERSION}-setup.run
 
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
