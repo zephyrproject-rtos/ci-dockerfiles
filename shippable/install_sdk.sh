1d872fddff4e4f5b87e1acf2c71921ae506a41a5
Kumar Gala <kumar.gala@linaro.org>
2020-05-06 21:48:34 -0500

    Install SDK 0.13.1-beta-1
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -7,3 +7,8 @@ chmod +x zephyr-sdk-${VERSION}-setup.run
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-setup.run
 
+wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.11.3-beta-1/zephyr-sdk-0.11.3-beta-1-setup.run
+chmod +x zephyr-sdk-0.11.3-beta-1-setup.run
+
+./zephyr-sdk-0.11.3-beta-1-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-0.11.3-beta-1
+rm ./zephyr-sdk-0.11.3-beta-1-setup.run
