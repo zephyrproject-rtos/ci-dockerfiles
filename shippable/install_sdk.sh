f6b2e3925ae632dc4e07f2cd500f8fdc95019a2c
Kumar Gala <kumar.gala@linaro.org>
2020-05-16 12:56:18 -0500

    sdk: Update to 0.11.3
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,14 +1,9 @@
 #!/bin/sh -e
 
-VERSION=0.11.2
+VERSION=0.11.3
 wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-setup.run
 chmod +x zephyr-sdk-${VERSION}-setup.run
 
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-setup.run
 
-wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.11.3-beta-1/zephyr-sdk-0.11.3-beta-1-setup.run
-chmod +x zephyr-sdk-0.11.3-beta-1-setup.run
-
-./zephyr-sdk-0.11.3-beta-1-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-0.11.3-beta-1
-rm ./zephyr-sdk-0.11.3-beta-1-setup.run
