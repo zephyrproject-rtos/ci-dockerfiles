75e2b6fe81bc9390a5988a352001bbb9a97b3070
Kumar Gala <kumar.gala@linaro.org>
2020-12-17 17:23:40 -0600

    Install SDK 0.12.0 in addition to 0.11.4
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -7,3 +7,9 @@ chmod +x zephyr-sdk-${VERSION}-setup.run
 ./zephyr-sdk-${VERSION}-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-setup.run
 
+VERSION=0.12.0
+wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-x86_64-linux-setup.run
+chmod +x zephyr-sdk-${VERSION}-x86_64-linux-setup.run
+
+./zephyr-sdk-${VERSION}-x86_64-linux-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+rm zephyr-sdk-${VERSION}-x86_64-linux-setup.run
