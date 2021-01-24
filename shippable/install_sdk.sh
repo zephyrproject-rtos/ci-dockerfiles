95c5d0173b354bb5da185d9f1768c5b55cb0e29e
Kumar Gala <kumar.gala@linaro.org>
2021-01-24 16:06:49 -0600

    Update SDK to 0.12.2 release
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-VERSION=0.12.1
+VERSION=0.12.2
 wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-x86_64-linux-setup.run
 chmod +x zephyr-sdk-${VERSION}-x86_64-linux-setup.run
 
