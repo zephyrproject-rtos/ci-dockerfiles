48372aebf8c3b9e672de7b966759a5a33a99edfb
Kumar Gala <kumar.gala@linaro.org>
2021-05-05 10:44:26 -0500

    0.13.0-alpha-1

--- shippable/install_sdk.sh
+++ shippable/install_sdk.sh
@@ -6,3 +6,10 @@ chmod +x zephyr-sdk-${VERSION}-x86_64-linux-setup.run
 
 ./zephyr-sdk-${VERSION}-x86_64-linux-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
 rm zephyr-sdk-${VERSION}-x86_64-linux-setup.run
+
+VERSION=0.13.0-alpha-1
+wget -q https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v${VERSION}/zephyr-sdk-${VERSION}-x86_64-linux-setup.run
+chmod +x zephyr-sdk-${VERSION}-x86_64-linux-setup.run
+
+./zephyr-sdk-${VERSION}-x86_64-linux-setup.run --quiet -- -d /opt/sdk/zephyr-sdk-${VERSION}
+rm zephyr-sdk-${VERSION}-x86_64-linux-setup.run
